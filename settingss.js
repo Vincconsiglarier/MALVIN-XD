8815cd71c6f75142c7b7f4903763fd62bd7c3ab6
Vincconsiglarier <vincconsiglarier@gmail.com>
2025-08-16 17:18:55 +0300

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "",  //malvin~E39FGaKJ#z8etYMZ6VCcgK5W5KxOzNV5sJM2Mh3H4ToBbw-Usz4A
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "255742928480", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
